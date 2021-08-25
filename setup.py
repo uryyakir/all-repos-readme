dd0e1765521888c83e3ec8c917c596e0071944df
Uri Yakir <uryyakir@gmail.com>
2021-08-25 17:33:19 +0300

    setup.py add required packages to install_requires parameter

--- setup.py
+++ setup.py
@@ -1,2 +1,6 @@
 from setuptools import setup
-setup()
+
+with open("requirements.txt", 'r') as reqs_file:
+    _install_requires = reqs_file.read().split("\n")
+
+setup(install_requires=_install_requires)
