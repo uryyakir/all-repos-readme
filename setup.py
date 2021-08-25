a64f1853c63d3c2d5e23a8e0b69e7a8e2ed9ef46
Uri Yakir <uryyakir@gmail.com>
2021-08-25 17:41:10 +0300

    setuptools provide full requirments.txt path

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
+import os
 from setuptools import setup
+# local modules
+from all_repos_add_readme.constants import PACKAGE_DIR
 
-with open("requirements.txt", 'r') as reqs_file:
+with open(os.path.join(PACKAGE_DIR, "requirements.txt"), 'r') as reqs_file:
     _install_requires = reqs_file.read().split("\n")
 
 setup(install_requires=_install_requires)
