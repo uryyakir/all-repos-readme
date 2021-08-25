4ff386988423c5f2a3fe12fce8832017dbf0bf34
Uri Yakir <uryyakir@gmail.com>
2021-08-25 16:57:33 +0300

    change pytest-ordering decorator signature to avoid unregistered markers

--- all_repos_add_readme/github_utils/tests/test_git_utils.py
+++ all_repos_add_readme/github_utils/tests/test_git_utils.py
@@ -6,25 +6,25 @@ from all_repos_add_readme.github_utils.git_utils import GitConfigHandler
 from conftest import Constants
 
 
-@pytest.mark.order1
+@pytest.mark.run(order=1)
 def test_getitem_does_not_exist(constants: Constants, git_config_handler_object: GitConfigHandler) -> None:
     with pytest.raises(NoSectionError):
         _ = git_config_handler_object[constants.TEST_PROPERTY_PATH]
 
 
-@pytest.mark.order2
+@pytest.mark.run(order=2)
 def test_delitem_does_not_exist(constants: Constants, git_config_handler_object: GitConfigHandler) -> None:
     assert not git_config_handler_object.__delitem__(constants.TEST_SECTION_NAME)
 
 
-@pytest.mark.order3
+@pytest.mark.run(order=3)
 def test_setitem(constants: Constants, git_config_handler_object: GitConfigHandler) -> None:
     git_config_handler_object[constants.TEST_PROPERTY_PATH] = constants.TEST_PROPERTY_VALUE
     with pytest.raises(Failed):  # sheeesh
         test_getitem_does_not_exist(**locals())
 
 
-@pytest.mark.order4
+@pytest.mark.run(order=4)
 def test_delitem(constants: Constants, git_config_handler_object: GitConfigHandler) -> None:
     assert git_config_handler_object.__delitem__(constants.TEST_SECTION_NAME)
     test_getitem_does_not_exist(constants, git_config_handler_object)
